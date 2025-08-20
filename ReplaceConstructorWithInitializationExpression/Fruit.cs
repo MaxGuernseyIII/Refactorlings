8e521f63e2c5f3344470188581fd0cf35a3ea686
Max Guernsey, III <max@hexsw.com>
2025-08-20 15:56:44 -0700

    .

--- ReplaceConstructorWithInitializationExpression/Fruit.cs
+++ ReplaceConstructorWithInitializationExpression/Fruit.cs
@@ -1,3 +1,17 @@
 ﻿namespace ReplaceConstructorWithInitializationExpression;
 
-record Fruit(string Name, double SizeInInches, double SugarInGrams, string Saying);
+record Fruit
+{
+  public Fruit(string Name, double SizeInInches, double SugarInGrams, string Saying)
+  {
+    this.Name = Name;
+    this.SizeInInches = SizeInInches;
+    this.SugarInGrams = SugarInGrams;
+    this.Saying = Saying;
+  }
+
+  public string Name { get; private set; }
+  public double SizeInInches { get; private set; }
+  public double SugarInGrams { get; private set; }
+  public string Saying { get; private set; }
+}
