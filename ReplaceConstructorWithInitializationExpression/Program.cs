bc57aaa7511cc66d2bf6df3de7d7d296c1513d82
Max Guernsey, III <max@hexsw.com>
2025-08-20 22:46:25 -0700

    .

--- ReplaceConstructorWithInitializationExpression/Program.cs
+++ ReplaceConstructorWithInitializationExpression/Program.cs
@@ -1,10 +1,8 @@
 ﻿using Refactorlings;
 
-var Point = new Complex(1, 0.1);
+var Point = new Complex(.1, .1);
 var Z0 = Point;
-var Z1 = Z0 * Z0 + 
-         Point;
-var Z2 = Z1 * Z1 +
-        Point;
+var Z1 = Z0 * Z0 + Point;
+var Z2 = Z1 * Z1 + Point;
 
 Console.WriteLine($"Z2 = {Z2}");
