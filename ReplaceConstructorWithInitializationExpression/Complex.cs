290302ab3a0b62a4e3aa0b323d07394eb6dcc522
Max Guernsey, III <max@hexsw.com>
2025-08-20 19:59:14 -0700

    .

--- ReplaceConstructorWithInitializationExpression/Complex.cs
+++ ReplaceConstructorWithInitializationExpression/Complex.cs
@@ -11,33 +11,46 @@ record Complex
   public double R { get; init; }
   public double I { get; init; }
 
-  public static Complex operator +(Complex Left, Complex Right)
+  public static Complex operator +(
+    Complex Left, Complex Right)
   {
-    return new(Left.R + Right.R, Left.I + Right.I);
+    return new(
+      Left.R + Right.R, 
+      Left.I + Right.I);
   }
 
-  public static Complex operator -(Complex Left, Complex Right)
+  public static Complex operator -(
+    Complex Left, 
+    Complex Right)
   {
-    return new(Left.R - Right.R, Left.I - Right.I);
+    return new(
+      Left.R - Right.R, 
+      Left.I - Right.I);
   }
 
-  public static Complex operator *(Complex Left, Complex Right)
+  public static Complex operator *(
+    Complex Left, Complex Right)
   {
     return new(
       Left.R * Right.R - Left.I * Right.I,
       Left.R * Right.I + Left.I * Right.R);
   }
 
-  public static Complex operator /(Complex Left, Complex Right)
+  public static Complex operator /(
+    Complex Left, Complex Right)
   {
-    var Denominator = Right.R * Right.R + Right.I * Right.I;
+    var Denominator = Right.R * Right.R + 
+                      Right.I * Right.I;
     return new(
-      (Left.R * Right.R + Left.I * Right.I) / Denominator,
-      (Left.I * Right.R - Left.R * Right.I) / Denominator
+      (Left.R * Right.R + 
+       Left.I * Right.I) / Denominator,
+      (Left.I * Right.R - 
+       Left.R * Right.I) / Denominator
     );
   }
 
-  public static implicit operator Complex(double R)
+  public static implicit operator Complex(
+    double R)
   {
     return new(R, 0);
   }
