c2c69acb7d378eab85a8105465f7831f7c1ec10a
Max Guernsey, III <max@hexsw.com>
2025-08-21 08:13:08 -0700

    .

--- ReplaceConstructorWithInitializationExpression/Position.cs
+++ ReplaceConstructorWithInitializationExpression/Position.cs
@@ -2,28 +2,10 @@
 // Copyright 2025-2025
 // All rights reserved.
 
-record Position
+record Position(
+  int Col,
+  int Line)
 {
-  public Position(
-    int Col,
-    int Line)
-  {
-    this.Col = Col;
-    this.Line = Line;
-  }
-
-  public int Col
-  {
-    get;
-    init;
-  }
-
-  public int Line
-  {
-    get;
-    init;
-  }
-
   public override
     string ToString()
   {
