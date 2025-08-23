18174da17e3774635af91c438090dec87d4bdfa6
Max Guernsey, III <max@hexsw.com>
2025-08-23 11:50:27 -0700

    .

--- Refactorlings234/Program.cs
+++ Refactorlings234/Program.cs
@@ -1,5 +1,9 @@
 ﻿// Refactorlings by Producore
 // © 2025 - All rights reserved
 
-Console.WriteLine(
-  "Hello, World!");
+var Left = new Complex(1, 1);
+var Right = new Complex(1, 1);
+
+var Product = Left * Right;
+
+Console.WriteLine(Product);
