ed188ba9cc2723d8c34721ba2398f8f12720583f
Max Guernsey, III <max@hexsw.com>
2025-08-23 13:39:52 -0700

    .

--- Refactorlings234/Complex.cs
+++ Refactorlings234/Complex.cs
@@ -1,8 +1,8 @@
 ﻿// Refactorlings by Producore
 // © 2025 - All rights reserved
 
-readonly record struct
-  Complex(float R, float I)
+readonly  struct Complex(
+  float R, float I)
 {
   readonly float I = I;
   readonly float R = R;
