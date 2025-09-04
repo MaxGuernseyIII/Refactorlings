05cc79e1f3a863b4a60957a3fc03f3b122853146
Max Guernsey, III <max@hexsw.com>
2025-09-04 14:57:01 -0700

    with refactored code for Refactoring 1 cover

---
+++ Mandelbrot/Mandelbrot.cs
@@ -0,0 +1,48 @@
+﻿// Refactorlings by Producore
+// © 2025 - All rights reserved
+
+public class Complex(float R, float I)
+{
+  public static readonly Complex Zero =
+    new(0, 0);
+
+  public float R { get; } = R;
+  public float I { get; } = I;
+
+  public Complex Square()
+  {
+    return new(R * R - I * I,
+      R * I * 2);
+  }
+
+  public float Abs()
+  {
+    return I * I + R * R;
+  }
+
+  public static Complex operator +(
+    Complex Left, Complex Right)
+  {
+    return new(Right.R + Left.R,
+      Right.I + Left.I);
+  }
+}
+
+public static class Mandelbrot
+{
+  public static bool TestEscape(
+    Complex C, int M = 100000)
+  {
+    var Z = Complex.Zero;
+    
+    for (var S = 0; S < M; S++)
+    {
+      Z = C + Z.Square();
+
+      if (Z.Abs() > 4)
+        return true;
+    }
+
+    return false;
+  }
+}
---
+++ Mandelbrot/Program.cs
@@ -0,0 +1,37 @@
+﻿// Refactorlings by Producore
+// © 2025 - All rights reserved
+
+Test(-2.001f, 0);
+Test(-1.999f, 0);
+Test(.249f, 0);
+Test(.251f, 0);
+Test(0, -.637f);
+Test(0, -.635f);
+Test(0, .635f);
+Test(0, .647f);
+
+void Test(float F, float F1)
+{
+  var Z = Mandelbrot.TestEscape(new Complex(F, F1));
+
+  Console.WriteLine($"Escape {F}, {F1}: {Z}");
+}
+
+var SearchWindow = (.630f, .640f);
+
+do
+{
+  var TestSpot = (SearchWindow.Item2 -
+                  SearchWindow.Item1) /
+                 2 + SearchWindow.Item1;
+
+  if (Mandelbrot.TestEscape(new Complex(0, TestSpot)))
+    SearchWindow = (SearchWindow.Item1,
+      TestSpot);
+  else
+    SearchWindow = (TestSpot,
+      SearchWindow.Item2);
+
+} while(SearchWindow.Item2 - SearchWindow.Item1 > 0.00001f);
+
+Console.WriteLine($"Search Window narrowed to {SearchWindow}");
